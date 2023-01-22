5f5146e4b50ed76af75da0bd589d2d2118fe745f
Arnav Tuli <arnavtuli00@gmail.com>
2023-01-22 13:40:04 +0530

    Add implementation for finding background and foreground inference

--- Assignment-1/backgroundSubtractor.py
+++ Assignment-1/backgroundSubtractor.py
@@ -29,22 +29,26 @@ class BackgroundSubtractor:
         self.variance = np.full((self.num_rows, self.num_cols, self.K), self.initial_variance, dtype=np.float32)
 
     # Trains background subtractor module on given dataset
-    #   dataset: list: list of images (except first one)
-    def train(self, dataset):
-        for i, image in enumerate(dataset):
-            print("Training iteration:", i + 1)
+    #   dataset:         list: list of images (except first one)
+    #   output_folder: string: path to save images
+    def train(self, dataset, output_folder):
+        for idx, image in enumerate(dataset):
+            print("Training iteration:", idx + 1)
             for i in range(self.num_rows):
                 for j in range(self.num_cols):
                     matching_gaussian = -1
                     for k in range(self.K):
-                        if np.linalg.norm(image[i][j] - self.mean[i][j][k]) < 2.5 * self.variance[i][j][k]:
+                        if np.linalg.norm(image[i][j] - self.mean[i][j][k]) < 2.5 * np.sqrt(self.variance[i][j][k]):
                             matching_gaussian = k
                             break
                     # no match, initialize new gaussian
                     if matching_gaussian == -1:
                         least_weight_gaussian = np.argmin(self.weight[i][j])
-                        self.mean[i][j][least_weight_gaussian] = image[i][j]
-                        self.variance[i][j][least_weight_gaussian] = self.initial_variance
+                        self.weight[i, j, :] = (1 - self.alpha) * self.weight[i, j, :]
+                        self.weight[i, j, least_weight_gaussian] += self.alpha
+                        self.mean[i, j, least_weight_gaussian] = image[i][j]
+                        self.mean_square[i, j, matching_gaussian] = np.linalg.norm(image[i][j]) ** 2
+                        self.variance[i, j, least_weight_gaussian] = self.initial_variance
                     # match found, update existing gaussian
                     else:
                         self.weight[i, j, :] = (1 - self.alpha) * self.weight[i, j, :]
@@ -52,7 +56,13 @@ class BackgroundSubtractor:
                         rho = self.alpha * self.gaussian_pdf(image[i][j].copy(), self.mean[i, j, matching_gaussian].copy(), self.variance[i, j, matching_gaussian].copy())
                         self.mean[i, j, matching_gaussian] = (1 - rho) * self.mean[i, j, matching_gaussian] + rho * image[i][j]
                         self.mean_square[i, j, matching_gaussian] = (1 - rho) * self.mean_square[i, j, matching_gaussian] + rho * (np.linalg.norm(image[i][j]) ** 2)
-                        self.variance[i, j, matching_gaussian] = self.mean_square[i, j, matching_gaussian] - (np.linalg.norm(self.mean[i, j, matching_gaussian]) ** 2)
+                        self.variance[i, j, matching_gaussian] = (1 - rho) * self.variance[i, j, matching_gaussian] + rho * np.abs(self.mean_square[i, j, matching_gaussian] - (np.linalg.norm(self.mean[i, j, matching_gaussian]) ** 2))
+                        if self.variance[i, j, matching_gaussian] < 1e-8:
+                            self.variance[i, j, matching_gaussian] = 1e-8
+                        # self.variance[i, j, matching_gaussian] = (1 - rho) * self.variance[i, j, matching_gaussian] + rho * (np.linalg.norm(image[i][j] - self.mean[i, j, matching_gaussian]) ** 2)
+            # self.find_background()
+            # self.inference([image], output_folder)
+        
     
     # Computes multi-dimensional gaussian pdf (scalar covariance matrix)
     #   X:        vector: input vector
@@ -64,8 +74,38 @@ class BackgroundSubtractor:
         covariance = variance * np.eye(self.num_channels)
         return np.exp(-0.5 * ((X - mean).T @ np.linalg.inv(covariance) @ (X - mean))) / ((np.sqrt(2 * np.pi) * variance) ** self.num_channels)
     
-    def inference(self, dataset):
-        pass
+    # Finds gaussians corresponding to background for each pixel
+    def find_background(self):
+        self.background = [[[] for _ in range(self.num_cols)] for _ in range(self.num_rows)]
+        for i in range(self.num_rows):
+            for j in range(self.num_cols):
+                bg_weights, bg_sum = [], 0
+                for k in range(self.K):
+                    bg_weights.append([self.weight[i][j][k] / np.sqrt(self.variance[i][j][k]), k])
+                bg_weights.sort(reverse=True)
+                for bg_weight in bg_weights:
+                    self.background[i][j].append(bg_weight[1])
+                    bg_sum += bg_weight[0]
+                    if bg_sum > self.T:
+                        break
+
+    # Filter foreground-background pixels in given dataset
+    #   dataset:         list: list of images
+    #   output_folder: string: path to save images
+    def inference(self, dataset, output_folder):
+        for idx, image in enumerate(dataset):
+            filtered_image = np.zeros((self.num_rows, self.num_cols, self.num_channels))
+            for i in range(self.num_rows):
+                for j in range(self.num_cols):
+                    matching_gaussian = -1
+                    for k in range(self.K):
+                        if np.linalg.norm(image[i][j] - self.mean[i][j][k]) < 2.5 * self.variance[i][j][k]:
+                            matching_gaussian = k
+                            break
+                    if matching_gaussian not in self.background[i][j]:
+                        filtered_image[i][j][0], filtered_image[i][j][1], filtered_image[i][j][2] = 255, 255, 255
+            print("Saving filtered image:", idx)
+            cv2.imwrite(output_folder + "out" + str(idx).zfill(6) + ".png", filtered_image)
         
 
                     
--- Assignment-1/main.py
+++ Assignment-1/main.py
@@ -9,13 +9,16 @@ def main():
     alpha = float(sys.argv[3])
     T = float(sys.argv[4])
     input_folder = os.getcwd() + "/" + "Dataset/" + folder + "/" + "input/"
+    output_folder = os.getcwd() + "/" + "Dataset/" + folder + "/" + "output/"
     input_files = [(input_folder + f) for f in os.listdir(input_folder)]
     input_files.sort()
     input_images = [cv2.imread(file) for file in input_files]
     initial_image = input_images[0]
     num_rows, num_cols, num_channels = initial_image.shape
-    model = BackgroundSubtractor(K, alpha, T, num_rows, num_cols, num_channels, 220.0, initial_image)
-    model.train(input_images)
+    model = BackgroundSubtractor(K, alpha, T, num_rows, num_cols, num_channels, 36, initial_image)
+    model.train(input_images[1:], output_folder)
+    model.find_background()
+    model.inference(input_images, output_folder)
 
 if __name__ == "__main__":
     main()
