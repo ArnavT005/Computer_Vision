f5403bf4ed9a644f6d69237b0e00d441ba841df1
Arnav Tuli <arnavtuli00@gmail.com>
2023-01-26 11:55:49 +0530

    Add vectorized implementation and foreground filtering

--- Assignment-1/backgroundSubtractor.py
+++ Assignment-1/backgroundSubtractor.py
@@ -9,23 +9,27 @@ class BackgroundSubtractor:
     #   num_rows:     int: number of rows in image
     #   num_cols:     int: number of columns in image
     #   num_channels: int: number of channels in image
-    def __init__(self, K, alpha, T, num_rows, num_cols, num_channels, initial_variance, initial_image):
+    def __init__(self, K, alpha, T, initial_data):
         # set model parameters
         self.K = K
         self.alpha = alpha
         self.T = T
-        self.num_rows = num_rows
-        self.num_cols = num_cols
-        self.num_channels = num_channels
-        self.initial_variance = initial_variance
-        self.initial_image = initial_image
+        self.initial_weight = initial_data["weight"]
+        self.initial_variance = initial_data["variance"]
+        self.initial_image = initial_data["image"]
+        self.num_rows, self.num_cols = self.initial_image.shape[:2]
+        if len(self.initial_image.shape) > 2:
+            self.num_channels = self.initial_image.shape[2]
+        else:
+            self.num_channels = 1
+            self.initial_image = self.initial_image.reshape([self.num_rows, self.num_cols, self.num_channels])
         # initialize MOG parameters
         self.mean = np.zeros((self.num_rows, self.num_cols, self.K, self.num_channels), dtype=np.float32)
-        self.mean[:, :, 0] = self.initial_image
+        self.mean[:, :, self.K - 1] = self.initial_image.copy()
         self.mean_square = np.zeros((self.num_rows, self.num_cols, self.K), dtype=np.float32)
-        self.mean_square[:, :, 0] = np.linalg.norm(self.initial_image, axis=2) ** 2
+        self.mean_square[:, :, self.K - 1] = np.linalg.norm(self.initial_image, axis=2) ** 2
         self.weight = np.zeros((self.num_rows, self.num_cols, self.K), dtype=np.float32)
-        self.weight[:, :, 0] = 1
+        self.weight[:, :, self.K - 1] = 1
         self.variance = np.full((self.num_rows, self.num_cols, self.K), self.initial_variance, dtype=np.float32)
 
     # Trains background subtractor module on given dataset
@@ -33,79 +37,93 @@ class BackgroundSubtractor:
     #   output_folder: string: path to save images
     def train(self, dataset, output_folder):
         for idx, image in enumerate(dataset):
-            print("Training iteration:", idx + 1)
-            for i in range(self.num_rows):
-                for j in range(self.num_cols):
-                    matching_gaussian = -1
-                    for k in range(self.K):
-                        if np.linalg.norm(image[i][j] - self.mean[i][j][k]) < 2.5 * np.sqrt(self.variance[i][j][k]):
-                            matching_gaussian = k
-                            break
-                    # no match, initialize new gaussian
-                    if matching_gaussian == -1:
-                        least_weight_gaussian = np.argmin(self.weight[i][j])
-                        self.weight[i, j, :] = (1 - self.alpha) * self.weight[i, j, :]
-                        self.weight[i, j, least_weight_gaussian] += self.alpha
-                        self.mean[i, j, least_weight_gaussian] = image[i][j]
-                        self.mean_square[i, j, matching_gaussian] = np.linalg.norm(image[i][j]) ** 2
-                        self.variance[i, j, least_weight_gaussian] = self.initial_variance
-                    # match found, update existing gaussian
-                    else:
-                        self.weight[i, j, :] = (1 - self.alpha) * self.weight[i, j, :]
-                        self.weight[i, j, matching_gaussian] += self.alpha
-                        rho = self.alpha * self.gaussian_pdf(image[i][j].copy(), self.mean[i, j, matching_gaussian].copy(), self.variance[i, j, matching_gaussian].copy())
-                        self.mean[i, j, matching_gaussian] = (1 - rho) * self.mean[i, j, matching_gaussian] + rho * image[i][j]
-                        self.mean_square[i, j, matching_gaussian] = (1 - rho) * self.mean_square[i, j, matching_gaussian] + rho * (np.linalg.norm(image[i][j]) ** 2)
-                        self.variance[i, j, matching_gaussian] = (1 - rho) * self.variance[i, j, matching_gaussian] + rho * np.abs(self.mean_square[i, j, matching_gaussian] - (np.linalg.norm(self.mean[i, j, matching_gaussian]) ** 2))
-                        if self.variance[i, j, matching_gaussian] < 1e-8:
-                            self.variance[i, j, matching_gaussian] = 1e-8
-                        # self.variance[i, j, matching_gaussian] = (1 - rho) * self.variance[i, j, matching_gaussian] + rho * (np.linalg.norm(image[i][j] - self.mean[i, j, matching_gaussian]) ** 2)
-            # self.find_background()
-            # self.inference([image], output_folder)
-        
+            if self.num_channels == 1:
+                image = image.reshape([self.num_rows, self.num_cols, self.num_channels])
+            match_found = np.full((self.num_rows, self.num_cols), False, dtype=bool)
+            for k in reversed(range(self.K)):
+                match_locations = np.where(np.logical_and(np.linalg.norm(image - self.mean[:, :, k], axis=-1) < 2.5 * np.sqrt(self.variance[:, :, k]), np.logical_not(match_found)))
+                if not match_locations[0].any():
+                    continue
+                match_found[match_locations] = True
+                weight_copy = self.weight[match_locations].copy()
+                weight_copy = (1 - self.alpha) * weight_copy
+                weight_copy[:, k] += self.alpha
+                self.weight[match_locations] = weight_copy.copy()
+                rho = self.alpha * self.gaussian_pdf(image[match_locations], self.mean[match_locations][:, k], self.variance[match_locations][:, k])
+                rho = rho.reshape(rho.shape[0], 1)
+                mean_copy = self.mean[match_locations].copy()
+                mean_copy[:, k] = (1 - rho) * mean_copy[:, k] + rho * image[match_locations]
+                self.mean[match_locations] = mean_copy.copy()
+                rho = rho.reshape(rho.shape[0],)
+                variance_copy = self.variance[match_locations].copy()
+                variance_copy[:, k] = (1 - rho) * variance_copy[:, k] + rho * (np.linalg.norm(image[match_locations] - self.mean[match_locations][:, k], axis=-1) ** 2)
+            no_match_locations = np.where(np.logical_not(match_found))
+            sort_weights = np.argsort(self.weight[no_match_locations], axis=-1)
+            self.weight[no_match_locations] = np.take_along_axis(self.weight[no_match_locations], sort_weights, axis=-1)
+            weight_copy = self.weight[no_match_locations].copy()
+            weight_copy[:, 0] = self.initial_weight
+            sum_weights = np.sum(weight_copy, axis=-1)
+            sum_weights = sum_weights.reshape(sum_weights.shape[0], 1)
+            weight_copy /= sum_weights
+            self.weight[no_match_locations] = weight_copy.copy()
+            mean_copy = self.mean[no_match_locations].copy()
+            for k in range(self.num_channels):
+                mean_copy[:, :, k] = np.take_along_axis(mean_copy[:, :, k], sort_weights, axis=-1)
+            mean_copy[:, 0] = image[no_match_locations].copy()
+            self.mean[no_match_locations] = mean_copy.copy()
+            self.variance[no_match_locations] = np.take_along_axis(self.variance[no_match_locations], sort_weights, axis=-1)
+            variance_copy = self.variance[no_match_locations].copy()
+            variance_copy[:, 0] = self.initial_variance
+            self.variance[no_match_locations] = variance_copy.copy()
+            sort_weights_by_std = np.argsort(self.weight / np.sqrt(self.variance), axis=-1)
+            self.weight = np.take_along_axis(self.weight, sort_weights_by_std, axis=-1)
+            mean_copy = self.mean.copy()
+            for k in range(self.num_channels):
+                mean_copy[:, :, :, k] = np.take_along_axis(mean_copy[:, :, :, k], sort_weights_by_std, axis=-1)
+            self.mean = mean_copy.copy()
+            self.variance = np.take_along_axis(self.variance, sort_weights_by_std, axis=-1)
+            foreground = self.find_foreground(image)
+            filtered_foreground = self.filter_foreground(foreground)
+            cv2.imwrite(output_folder + "out" + str(0).zfill(6) + ".png", filtered_foreground)
     
     # Computes multi-dimensional gaussian pdf (scalar covariance matrix)
-    #   X:        vector: input vector
-    #   mean:     vector: mean vector
-    #   variance: scalar: variance along a component
+    #   X:        ndarray[B, num_channels]: input vector
+    #   mean:     ndarray[B, num_channels]: mean vector
+    #   variance:              ndarray[B,]: variance along a component
     def gaussian_pdf(self, X, mean, variance):
-        X = np.reshape(X, (self.num_channels, 1))
-        mean = np.reshape(mean, (self.num_channels, 1))
-        covariance = variance * np.eye(self.num_channels)
-        return np.exp(-0.5 * ((X - mean).T @ np.linalg.inv(covariance) @ (X - mean))) / ((np.sqrt(2 * np.pi) * variance) ** self.num_channels)
+        return np.exp(-0.5 * (np.linalg.norm(X - mean, axis=-1) ** 2) / variance) / ((np.sqrt(2 * np.pi * variance)) ** self.num_channels)
     
-    # Finds gaussians corresponding to background for each pixel
-    def find_background(self):
-        self.background = [[[] for _ in range(self.num_cols)] for _ in range(self.num_rows)]
-        for i in range(self.num_rows):
-            for j in range(self.num_cols):
-                bg_weights, bg_sum = [], 0
-                for k in range(self.K):
-                    bg_weights.append([self.weight[i][j][k] / np.sqrt(self.variance[i][j][k]), k])
-                bg_weights.sort(reverse=True)
-                for bg_weight in bg_weights:
-                    self.background[i][j].append(bg_weight[1])
-                    bg_sum += bg_weight[0]
-                    if bg_sum > self.T:
-                        break
+    # Finds pixels corresponding to foreground
+    #   image:  ndarray[R, C, num_channels]: input image
+    def find_foreground(self, image):
+        foreground = np.zeros((self.num_rows, self.num_cols), dtype=np.uint8)
+        match_found = np.full((self.num_rows, self.num_cols), False, dtype=bool) 
+        cumulative_sum = np.zeros((self.num_rows, self.num_cols), dtype=np.float32)
+        for k in reversed(range(self.K)):
+            match_locations = np.where(np.logical_and(np.logical_and(np.linalg.norm(image - self.mean[:, :, k], axis=-1) < 2.5 * np.sqrt(self.variance[:, :, k]), np.logical_not(match_found)), cumulative_sum < self.T))
+            match_found[match_locations] = True
+            cumulative_sum += self.weight[:, :, k].copy()
+        no_match_locations = np.where(np.logical_not(match_found))
+        foreground[no_match_locations] = 255
+        return foreground
+    
+    def filter_foreground(self, foreground):
+        numLabels, labels, stats, _ = cv2.connectedComponentsWithStats(foreground, 8, cv2.CV_32S)
+        filtered_foreground = np.zeros((foreground.shape), dtype=np.uint8)
+        for i in range(1, numLabels):
+            w = stats[i, cv2.CC_STAT_WIDTH]
+            h = stats[i, cv2.CC_STAT_HEIGHT]
+            area = stats[i, cv2.CC_STAT_AREA]
+            # if w > 150:
+            #     continue
+            # if area < 900 and (area / (w * h)) < 0.5:
+            #     continue
+            # if area < 200:
+            #     continue
+            filtered_foreground += (labels == i).astype("uint8") * 255
+        return filtered_foreground
 
-    # Filter foreground-background pixels in given dataset
-    #   dataset:         list: list of images
-    #   output_folder: string: path to save images
-    def inference(self, dataset, output_folder):
-        for idx, image in enumerate(dataset):
-            filtered_image = np.zeros((self.num_rows, self.num_cols, self.num_channels))
-            for i in range(self.num_rows):
-                for j in range(self.num_cols):
-                    matching_gaussian = -1
-                    for k in range(self.K):
-                        if np.linalg.norm(image[i][j] - self.mean[i][j][k]) < 2.5 * self.variance[i][j][k]:
-                            matching_gaussian = k
-                            break
-                    if matching_gaussian not in self.background[i][j]:
-                        filtered_image[i][j][0], filtered_image[i][j][1], filtered_image[i][j][2] = 255, 255, 255
-            print("Saving filtered image:", idx)
-            cv2.imwrite(output_folder + "out" + str(idx).zfill(6) + ".png", filtered_image)
+
         
 
                     
--- Assignment-1/main.py
+++ Assignment-1/main.py
@@ -2,23 +2,33 @@ import os
 import cv2
 import sys
 from backgroundSubtractor import BackgroundSubtractor
+import numpy as np
+
+def norm_pdf(x,mean,sigma):
+        return (1/(np.sqrt(2*3.14)*sigma))*(np.exp(-0.5*(((x-mean)/sigma)**2)))
 
 def main():
     folder = sys.argv[1]
     K = int(sys.argv[2])
     alpha = float(sys.argv[3])
     T = float(sys.argv[4])
+    initial_weight = float(sys.argv[5])
+    initial_variance = float(sys.argv[6])
     input_folder = os.getcwd() + "/" + "Dataset/" + folder + "/" + "input/"
     output_folder = os.getcwd() + "/" + "Dataset/" + folder + "/" + "output/"
     input_files = [(input_folder + f) for f in os.listdir(input_folder)]
     input_files.sort()
+    # input_images = [cv2.cvtColor(cv2.imread(file), cv2.COLOR_BGR2GRAY) for file in input_files]
     input_images = [cv2.imread(file) for file in input_files]
     initial_image = input_images[0]
-    num_rows, num_cols, num_channels = initial_image.shape
-    model = BackgroundSubtractor(K, alpha, T, num_rows, num_cols, num_channels, 36, initial_image)
+    initial_data = {
+        "weight": initial_weight,
+        "variance": initial_variance,
+        "image": initial_image
+    }
+    model = BackgroundSubtractor(K, alpha, T, initial_data)
     model.train(input_images[1:], output_folder)
-    model.find_background()
-    model.inference(input_images, output_folder)
+
 
 if __name__ == "__main__":
     main()
