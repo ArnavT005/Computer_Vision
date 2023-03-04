37ee04791443b21a61f0bd853ab9424c4fe9f5fe
Arnav Tuli <arnavtuli00@gmail.com>
2023-03-04 16:56:40 +0530

    main.py complete

--- Assignment-2/main.py
+++ Assignment-2/main.py
@@ -1,62 +1,51 @@
+import os
 import cv2
+import argparse
+import numpy as np
 from imageStitcher import ImageStitcher
 from cornerDetector import CornerDetector
-import numpy as np
 
-imageStitcher = ImageStitcher(1000, 2, 100)
-window = {
-    "type": "gaussian",
-    "size": 5,
-}
-cornerDetector = CornerDetector(window, 0.04, 0.01, 2)
-img_list = [cv2.imread(f"Dataset/1/image {i}.jpg") for i in range(15)]
-while len(img_list) > 1:
-    temp_img_list = []
-    for i in range(len(img_list) // 2):
-        img = img_list[2 * i]
-        dst = cornerDetector.find(img)
-        img_ = img_list[2 * i + 1]
-        dst_ = cornerDetector.find(img_)
-        src, dest = imageStitcher.match((img_, img), (dst_, dst))
-        h = imageStitcher.getAffineTransform(src, dest)
-        result = cv2.warpPerspective(img_, h, (img_.shape[1] + img.shape[1], img_.shape[0]))
-        result[0:img.shape[0], 0:img.shape[1]] = img.copy()
+parser = argparse.ArgumentParser()
+parser.add_argument("--dir", type=str, default="1")
+parser.add_argument("--p_radius", type=int, default=1000)
+parser.add_argument("--n_radius", type=int, default=2)
+parser.add_argument("--sample_count", type=int, default=100)
+parser.add_argument("--window_size", type=int, default=5)
+parser.add_argument("--k", type=float, default=0.04)
+parser.add_argument("--thresh", type=float, default=0.01)
+parser.add_argument("--nms_radius", type=int, default=2)
+parser.add_argument("--gaussian", default=True, action=argparse.BooleanOptionalAction)
+
+args = parser.parse_args()
+
+
+def main():
+    imageStitcher = ImageStitcher(args.p_radius, args.n_radius, args.sample_count)
+    window = {
+        "type": "gaussian" if args.gaussian else "box",
+        "size": args.window_size
+    }
+    cornerDetector = CornerDetector(window, args.k, args.thresh, args.nms_radius)
+    data_dir = os.getcwd() + "/" + "Dataset/" + args.dir + "/"
+    img_files = [(data_dir + "image " + str(i) + ".jpg") for i in range(len(os.listdir(data_dir)))]
+    img_list = [cv2.imread(file) for file in img_files]
+    img_left = img_list[0].copy()
+    pts_left = cornerDetector.find(img_left)
+    for i in range(1, len(img_list)):
+        img_right = img_list[i]
+        pts_right = cornerDetector.find(img_right)
+        src, dest = imageStitcher.match((img_right, img_left), (pts_right, pts_left))
+        affine = imageStitcher.getAffineTransform2D(src, dest)
+        result = cv2.warpAffine(img_right, affine, (img_right.shape[1] + img_left.shape[1], img_left.shape[0]))
+        result[0:img_left.shape[0], 0:img_left.shape[1]] = img_left.copy()    
         zero_idx = np.argwhere(np.all(result[..., :] == 0, axis=0))
         result = result[:, :zero_idx[0][0]].copy()
-        temp_img_list.append(result)
-        # breakpoint()
-    if len(img_list) % 2 == 1:
-        temp_img_list.append(img_list[-1].copy())
-    img_list = temp_img_list
-breakpoint()
-# dst = cornerDetector.find(img)
-# dst = cv2.cornerHarris(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 5, 3, 0.2)
-# dst = cv2.dilate(dst * 1.0,None)
-# img[dst > 0] = [0, 0, 255]
-# for i in range(1, 15):
-#     img_ = cv2.imread(f"Dataset/2/image {6}.jpg")
-#     dst_ = cornerDetector.find(img_)
-#     src, dest = imageStitcher.match((img_, img), (dst_, dst))
-#     h = imageStitcher.getAffineTransform(src, dest)
-#     result = cv2.warpPerspective(img_, h, (img_.shape[1] + img.shape[1], img_.shape[0]))
-#     # cv2.imwrite("persp.png", result)
-#     # src_array = np.concatenate((np.array(src), np.ones((len(src), 1))), axis=-1)
-#     # dest_array = np.array(dest)
-#     # affine = (np.linalg.inv(src_array.T @ src_array) @ src_array.T @ dest_array).T
-#     # result = cv2.warpAffine(img_, affine, (img_.shape[1] + img.shape[1], img_.shape[0]))
-#     # cv2.imwrite("affine.png", result)
-#     result[0:img.shape[0], 0:img.shape[1]] = img.copy()    
-#     cv2.imwrite("sample.png", result)
-#     breakpoint()
-#     zero_idx = np.argwhere(np.all(result[..., :] == 0, axis=0))
-#     # result = np.delete(result, zero_idx, axis=1)
-#     result = result[:, :zero_idx[0][0]].copy()
-#     cv2.imwrite("crop.png", result)
-#     breakpoint()
-#     img = result.copy()
-#     dst = cornerDetector.find(img)
-# breakpoint()
-# print(((dst > 0) * 1).sum())
-# dst = cv2.dilate(dst,None)
-# img[dst > 0] = [0, 0, 255]
-# cv2.imwrite("harris_our.png", img)
+        img_left = result.copy()
+        pts_left = cornerDetector.find(img_left)
+        print(f"Stitched image {i} to base...")
+    print("Image stitching complete. Saving final image as stitched-" + args.dir + ".jpg")
+    cv2.imwrite("stitched-" + args.dir + ".jpg", img_left)
+
+
+if __name__ == "__main__":
+    main()
